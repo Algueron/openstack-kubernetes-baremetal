e3f6f535b4693da93680df4b31959bb97efccacd
Julien Papillon <julien.papillon@gmail.com>
2025-05-03 12:03:00 +0200

    feat: Create custom flavors for Kubernetes

--- opentofu/project/main.tf
+++ opentofu/project/main.tf
@@ -34,3 +34,27 @@ resource "openstack_compute_quotaset_v2" "quotaset_1" {
   security_groups      = 20
   security_group_rules = 200
 }
+
+resource "openstack_compute_flavor_v2" "kubernetes-control-flavor" {
+  name  = "kubernetes-control"
+  vcpus = var.control_node_vcpus
+  ram   = var.control_node_memory_mb
+  disk  = var.control_node_root_disk_size_gb
+}
+
+resource "openstack_compute_flavor_access_v2" "control-flavor-access" {
+  tenant_id = openstack_identity_project_v3.os_project.id
+  flavor_id = openstack_compute_flavor_v2.kubernetes-control-flavor.id
+}
+
+resource "openstack_compute_flavor_v2" "kubernetes-worker-flavor" {
+  name  = "kubernetes-worker"
+  vcpus = var.worker_node_vcpus
+  ram   = var.worker_node_memory_mb
+  disk  = var.worker_node_root_disk_size_gb
+}
+
+resource "openstack_compute_flavor_access_v2" "worker-flavor-access" {
+  tenant_id = openstack_identity_project_v3.os_project.id
+  flavor_id = openstack_compute_flavor_v2.kubernetes-worker-flavor.id
+}
--- opentofu/project/variables.tf
+++ opentofu/project/variables.tf
@@ -14,3 +14,39 @@ variable "os_password" {
     description = "Openstack user password"
     type        = string
 }
+
+variable "control_node_vcpus" {
+    description = "Number of vCPUs for control node flavor"
+    type        = number
+    default     = 2
+}
+
+variable "control_node_memory_mb" {
+    description = "Amount of RAM to use, in megabytes, for control node flavor"
+    type        = number
+    default     = 4096
+}
+
+variable "control_node_root_disk_size_gb" {
+    description = "The amount of disk space in GiB to use for the root (/) partition for control node flavor"
+    type        = number
+    default     = 30
+}
+
+variable "worker_node_vcpus" {
+    description = "Number of vCPUs for worker node flavor"
+    type        = number
+    default     = 8
+}
+
+variable "worker_node_memory_mb" {
+    description = "Amount of RAM to use, in megabytes, for worker node flavor"
+    type        = number
+    default     = 32768
+}
+
+variable "worker_node_root_disk_size_gb" {
+    description = "The amount of disk space in GiB to use for the root (/) partition for worker node flavor"
+    type        = number
+    default     = 50
+}
